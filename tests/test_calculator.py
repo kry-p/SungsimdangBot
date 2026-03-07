ecb7d2a7313eadf107fa338c28fa607fb4df132f
Jaehun Jung <jhjung.dev@gmail.com>
2026-03-07 15:22:58 +0900

    test: add tests for calculator internal methods and edge cases

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -69,6 +69,86 @@ class TestErrors:
         assert calc.operation("") == "syntax error"
 
 
+class TestStringToNumber:
+    def test_integer(self, calc):
+        assert calc.string_to_number("42") == 42
+        assert isinstance(calc.string_to_number("42"), int)
+
+    def test_float(self, calc):
+        assert calc.string_to_number("3.14") == 3.14
+        assert isinstance(calc.string_to_number("3.14"), float)
+
+    def test_constant_pi(self, calc):
+        assert calc.string_to_number("pi") == math.pi
+
+    def test_constant_e(self, calc):
+        assert calc.string_to_number("e") == math.e
+
+    def test_non_numeric_string(self, calc):
+        assert calc.string_to_number("+") == "+"
+        assert calc.string_to_number("sqrt") == "sqrt"
+
+
+class TestWrongSyntaxChecker:
+    def test_valid_expression(self, calc):
+        assert calc.wrong_syntax_checker("2 + 3 * 4") is None
+
+    def test_valid_with_constants(self, calc):
+        assert calc.wrong_syntax_checker("pi + e") is None
+
+    def test_valid_with_functions(self, calc):
+        assert calc.wrong_syntax_checker("sqrt(4)") is None
+
+    def test_invalid_characters(self, calc):
+        assert calc.wrong_syntax_checker("2 + abc") == "syntax error"
+
+    def test_invalid_single_letter(self, calc):
+        assert calc.wrong_syntax_checker("x") == "syntax error"
+
+
+class TestPriority:
+    def test_function_priority(self, calc):
+        assert calc.priority("sqrt") == 0
+        assert calc.priority("sin") == 0
+
+    def test_mul_div_pow_priority(self, calc):
+        assert calc.priority("*") == 1
+        assert calc.priority("/") == 1
+        assert calc.priority("^") == 1
+
+    def test_add_sub_priority(self, calc):
+        assert calc.priority("+") == 2
+        assert calc.priority("-") == 2
+
+    def test_parentheses_priority(self, calc):
+        assert calc.priority("(") == 3
+        assert calc.priority(")") == 3
+
+    def test_unknown_priority(self, calc):
+        assert calc.priority("xyz") == 4
+
+
+class TestOperationEdgeCases:
+    def test_decimal_arithmetic(self, calc):
+        assert calc.operation(" 0.1 + 0.2") == round(0.1 + 0.2, 4)
+
+    def test_constants_combination(self, calc):
+        assert calc.operation(" pi + e") == round(math.pi + math.e, 4)
+
+    def test_power_precedence(self, calc):
+        assert calc.operation(" 2 ^ 3 * 2") == 16
+
+    @pytest.mark.xfail(reason="consecutive negative operands not supported", strict=True)
+    def test_negative_times_negative(self, calc):
+        assert calc.operation(" -2 * -3") == 6
+
+    def test_large_number(self, calc):
+        assert calc.operation(" 999999 * 999999") == 999998000001
+
+    def test_only_whitespace(self, calc):
+        assert calc.operation("   ") == "syntax error"
+
+
 class TestFunctionCalls:
     @pytest.mark.xfail(reason="str.insert() bug in tokenize() line 80", strict=True)
     def test_sqrt(self, calc):
